3e74cf50fa250ac66c04e1a661caa4ffd830288f
Rasmus Rudling <rasmusrudling@gmail.com>
2025-02-24 14:14:01 +0100

    security lesson 5

---
+++ lecture5/src/bike-store-repository.test.ts
@@ -0,0 +1,18 @@
+import {describe, expect, it, jest, test} from "@jest/globals";
+import {BikeStoreRepository} from "./bike-store-repository";
+import 'dotenv/config';
+
+const sum = (a: number, b: number) => {
+  return a + b
+}
+
+describe('ShouldSumProperly', () => {
+  test.each([
+    [1, 2, 3],
+    [4, 4, 8],
+    [9, 9, 18],
+    [10, 10, 20]
+  ])("adds %i + %i to equal %i", (firstNum, secondNum, expected) => {
+    expect(sum(firstNum, secondNum)).toBe(expected);
+  });
+});
